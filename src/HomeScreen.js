d05bfb3b1e242833964fdac81cc64a2eae19ad93
parth.khunteta <pkhunt@icims.com>
2021-02-11 14:30:02 +0530

    NB:01 Added NavBar

---
+++ src/HomeScreen.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./HomeScreen.css";
+import Nav from "./Nav";
+function HomeScreen() {
+  return (
+    <div className="homeScreen">
+      <Nav />
+
+      {/*BANNER*/}
+
+      {/*Row*/}
+    </div>
+  );
+}
+
+export default HomeScreen;
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyACSlLUfjKAPN0eBBvIK7N8qmbbthc08WU",
+  authDomain: "netflix-clone-c38bc.firebaseapp.com",
+  projectId: "netflix-clone-c38bc",
+  storageBucket: "netflix-clone-c38bc.appspot.com",
+  messagingSenderId: "144260089115",
+  appId: "1:144260089115:web:fa6f8067d7892010593fbe",
+};
